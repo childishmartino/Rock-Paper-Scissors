a73be46c728d851817189daa0d881b9439a26121
childishmartino <76404189+childishmartino@users.noreply.github.com>
2021-03-03 19:05:20 -0800

    project finished

--- script.js
+++ script.js
@@ -1,44 +1,43 @@
-var userChoice = prompt("Do you choose R, P, or S?");
 var options = ['R', 'P', 'S'];
-var index = Math.floor(Math.random() * options.length);
-var computerChoice = options[index];
+var wins = 0
+var loss = 0
+var ties = 0
 
-var compare = function (userChoice, computerChoice) {
-    if (userChoice === computerChoice) {
-        alert("You tied! Try again.");
-        return "tie";
+var playGame = function () {
+    var userChoice = prompt("Do you choose R, P, or S?");
+    if (!userChoice) {
+        return;
     }
-    else {
-        if (userChoice === "R") {
-            if (computerChoice === "S") {
-                alert("Winner Winner Chicken Dinner");
-                return "R wins";
-            }
-            else {
-                alert("Better luck next time.");
-                return "loser";
-            }
-        }
-        if (userChoice === "P") {
-            if (computerChoice === "R") {
-                alert("Winner")
-                return "S wins";
-            }
-            else {
-                alert ("Better luck next time.");
-                return "loser";
-            }
-        }
-        if (userChoice === "S") {
-            if (computerChoice === "P") {
-                alert("Winner Winner Chicken Dinner");
-                return "P wins";
-            }
-            else {
-                alert("Better luck next time.");
-                return "loser";
-            }
-        }
+    
+    userChoice = userChoice.toUpperCase();
+
+    var index = Math.floor(Math.random() * options.length);
+
+    var computerChoice = options[index];
+
+    window.alert("The computer chose " + computerChoice)
+
+    if (userChoice === computerChoice) {
+        ties ++;
+        alert("You tied! Try again.");
+} else if (
+    (userChoice === "R" && computerChoice === "S") ||
+    (userChoice === "S" && computerChoice === "P") ||
+    (userChoice === "P" && computerChoice ==="R")
+) {
+    wins ++;
+    alert("Winner");
+} else {
+    loss ++;
+    alert("loser");
+}
+
+    alert("stats:\n Wins: " + wins + "\n Losses: " + loss + "\n Ties: " + ties);
+
+    var playAgain = confirm ("Do you want to play again");
+    if (playAgain) {
+        playGame()
     }
 }
-var result = compare(userChoice,computerChoice);
+
+playGame();
